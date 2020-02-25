1007f2128b3ef58d9ee7cbf2eff33549138c984b
DedovetsNS <kokocik3@bk.ru>
2020-02-25 13:46:36 +0300

    added declare queue

--- src/main/java/mail/sender/amqp/rabbit/config/MessageConverterConfig.java
+++ src/main/java/mail/sender/amqp/rabbit/config/MessageConverterConfig.java
@@ -1,11 +1,19 @@
 package mail.sender.amqp.rabbit.config;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+
 @Configuration
 public class MessageConverterConfig {
+
+    @Bean
+    public Queue queue() {
+        return new Queue("query-library-mail-send");
+    }
 
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
--- src/main/java/mail/sender/amqp/rabbit/listener/LibraryListener.java
+++ src/main/java/mail/sender/amqp/rabbit/listener/LibraryListener.java
@@ -2,30 +2,39 @@ package mail.sender.amqp.rabbit.listener;
 
 import mail.sender.dto.MessageDto;
 import mail.sender.service.EmailService;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.support.converter.DefaultClassMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @EnableRabbit
 @Component
 public class LibraryListener {
 
     private final EmailService emailService;
+    private final AmqpAdmin amqpAdmin;
 
     @Autowired
-    public LibraryListener(EmailService emailService) {
+    public LibraryListener(EmailService emailService, AmqpAdmin amqpAdmin) {
         this.emailService = emailService;
+        this.amqpAdmin = amqpAdmin;
     }
 
     @RabbitListener(queues = "query-library-mail-send")
-    public void sendMailFromLibrary(MessageDto message) throws InterruptedException {
+    public void sendMailFromLibrary(MessageDto message)  {
         emailService.sendSimpleMessage(
                 message.getTo(),
                 message.getSubject(),
                 message.getText());
 
+    }
+
+    @PostConstruct
+    public void declareQueue(){
+        amqpAdmin.declareQueue(new Queue("query-library-mail-send"));
     }
 }
